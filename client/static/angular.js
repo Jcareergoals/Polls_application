362c4c214a893e7f4ca94744fdf21ae1a50aff02
Jose Chery <Jcareergoals@hotmail.com>
2016-05-05 02:24:13 -0400

    added features to create and delete polls. Only users that created a poll can delete their poll. Added filter feature to dashboard to filter through polls

--- client/static/angular.js
+++ client/static/angular.js
@@ -6,6 +6,7 @@ var mean = angular.module('mean', ['ngRoute']);
 			.when('/', {templateUrl:"./static/partials/login.html"}) 
 			.when('/dashboard', {templateUrl:"./static/partials/dashboard.html"})
 			.when('/create', {templateUrl:"./static/partials/createPoll.html"})
+			.when('/poll/:id',{templateUrl:"./static/partials/polls.html"})
 			.otherwise({redirectTo:"/"})
 
 	})
@@ -34,13 +35,33 @@ var mean = angular.module('mean', ['ngRoute']);
 	mean.factory('pollFactory', function($http, $window){
 		var factory = {}; 
 		factory.create = function(data){
-			$http.post('/poll', data).success(function(data){
+			$http.post('/polls', data).success(function(data){
 				if(data.status == 'success'){
 					console.log('Poll was successfully created'); 
 					$window.location.href='#/dashboard'; 
 				} else {
 					console.log('There was an error creating the poll'); 
 				}
+			}); 
+		}
+		factory.index = function(callback){
+			$http.get('/polls').success(function(data){
+				callback(data); 
+			}); 
+		}
+		factory.getPoll = function(data, callback){
+			$http.get('/poll/'+data.id).success(function(data){
+				callback(data);
+			}); 
+		}
+		factory.vote = function(data, callback){
+			$http.post('/vote', data).success(function(data){
+				callback(data);
+			});
+		}
+		factory.delete = function(data, callback){
+			$http.post('/delete', data).success(function(data){
+				callback(data); 
 			}); 
 		}
 		return factory; 
@@ -52,11 +73,38 @@ var mean = angular.module('mean', ['ngRoute']);
 			loginFactory.create($scope.session)
 		}
 	}); 
-	mean.controller('dashboard', function($scope, dashboardFactory){
+	mean.controller('dashboard', function($scope, pollFactory){
+		$scope.polls = []; 
+		pollFactory.index(function(data){
+			console.log(data); 
+			$scope.polls = data; 
+		}); 
+		$scope.delete = function(data){
+			pollFactory.delete(data, function(data){
+				console.log(data);
+				$scope.polls = data; 
+			}); 
+		}
 	}); 
 	mean.controller('createPoll', function($scope, pollFactory){
 		$scope.createPoll = function(){
 			console.log($scope.poll); 
 			pollFactory.create($scope.poll); 
+		}
+	}); 
+	mean.controller('polls', function($scope, pollFactory, $routeParams){
+		pollFactory.getPoll($routeParams, function(data){
+			console.log(data); 
+			$scope.survey = data; 
+		}); 
+		$scope.vote = function(data){
+			var newVote = {
+				id: $scope.survey._id, 
+				option: data
+			} 
+			pollFactory.vote(newVote, function(data){
+				$scope.survey = data; 
+				newVote = {};
+			}); 
 		}
 	}); 
--- server.js
+++ server.js
@@ -50,10 +50,12 @@ app
 			res.json({status:'error'})
 		}
 	})
-	.post('/poll', function(req, res){
+	.post('/polls', function(req, res){
 		if(req.body.question){
 			// console.log(req.body)
 			var poll = new Poll()
+			poll.question = req.body.question
+			poll.created_by = req.session.name
 			poll.option_1.name = req.body.option_1
 			poll.option_2.name = req.body.option_2
 			poll.option_3.name = req.body.option_3
@@ -63,6 +65,58 @@ app
 		} else {
 			res.json({status:'error'})
 		}
+	})
+	.post('/vote', function(req, res){
+		console.log(req.body.option)
+		if(req.body.option === 1){
+			Poll.update({_id:req.body.id}, {$inc:{"option_1.count": 1}}, function(err, data){
+				console.log(err, data)
+			})
+		} else if (req.body.option === 2){
+			Poll.update({_id:req.body.id}, {$inc:{"option_2.count": 1}}, function(err, data){
+				console.log(err, data)
+			})
+		} else if (req.body.option === 3){
+			Poll.update({_id:req.body.id}, {$inc:{"option_3.count": 1}}, function(err, data){
+				console.log(err, data)
+			})
+		} else if (req.body.option === 4){
+			Poll.update({_id:req.body.id}, {$inc:{"option_4.count": 1}}, function(err, data){
+				console.log(err, data)
+			})
+		}
+		Poll.findOne({_id:req.body.id}, function(err, data){
+			// console.log(data)
+			res.json(data)
+		})
+	})
+	.post('/delete', function(req, res){
+		// console.log(req.body)
+		if(req.session.name == req.body.created_by){
+			Poll.remove({_id:req.body.id}, function(err, data){
+				console.log('This poll was removed')
+			})
+			Poll.find({}, function(err, data){
+				res.json(data)
+			})
+		} else {
+			Poll.find({}, function(err, data){
+				console.log('You cannot delete this file') 
+				res.json(data)
+			})
+		}
+
+	})
+	.get('/polls', function(req, res){
+		Poll.find({}, function(err, data){
+			res.json(data)
+		})
+	})
+	.get('/poll/:id', function(req, res){
+		Poll.findOne({_id:req.params.id}, function(err, data){
+			console.log(data)
+			res.json(data)
+		})
 	})
 
 app.listen(8000, function(){
